e181b868dfdf5c9503dd8fddbed8a02f67179d03
juniorti91 <juniortidesenvolvimento1@gmail.com>
2024-06-10 23:59:10 -0300

    Aula 09 Em andamento

---
+++ pro-atividade-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const atividade = [
+    {
+      "id": 1,
+      "descricao": "Primeira Atividade"
+    }
+  ]
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          <li>{atividade[0].id} {atividade[0].descricao}</li>
+          <li onClick={() => console.log('JSX')}>Segunda Atividade</li>
+          <li>Terceira Atividade</li>
+          <li>Quarta Atividade</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
