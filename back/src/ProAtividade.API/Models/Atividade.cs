9f2c73f239cff3d45fcfc99ce8b5b60cb8197d70
juniorti91 <juniortidesenvolvimento1@gmail.com>
2024-06-19 14:13:42 -0300

    Aula 48 em andamento

---
+++ back/src/ProAtividade.API/Models/Atividade.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAtividade.API.Models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+    }
+}
