cfbf75b89e0a9e3eaec3e7379d99bce0d2b32b5c
juniorti91 <juniortidesenvolvimento1@gmail.com>
2024-06-21 20:11:11 -0300

    Salvo

--- back/src/ProAtividade.API/Controllers/AtividadeController.cs
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Data;
 using ProAtividade.API.Models;
 
 namespace ProAtividade.API.Controllers
@@ -11,29 +12,34 @@ namespace ProAtividade.API.Controllers
     [Route("api/[controller]")]
     public class AtividadeController : ControllerBase
     {
-        public IEnumerable<Atividade> Atividades = new List<Atividade> () 
+        private readonly DataContext _context;
+
+        public AtividadeController(DataContext context)
         {
-            new Atividade(1),
-            new Atividade(2),
-            new Atividade(3)
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public IEnumerable<Atividade> get() 
         {
-            return Atividades;
+            return _context.Atividades;
         }
 
         [HttpGet("{id}")]
         public Atividade get(int id) 
         {
-            return Atividades.FirstOrDefault(ativ => ativ.Id == id);
+            return _context.Atividades.FirstOrDefault(ativ => ativ.Id == id);
         }
 
         [HttpPost]
         public IEnumerable<Atividade> post(Atividade atividade) 
         {  
-            return Atividades.Append<Atividade>(atividade);
+            _context.Atividades.Add(atividade);
+            if (_context.SaveChanges() > 0) {
+                return _context.Atividades;
+            }
+
+            return _context.Atividades.Append<Atividade>(atividade);
         }
 
         [HttpPut("{id}")]
